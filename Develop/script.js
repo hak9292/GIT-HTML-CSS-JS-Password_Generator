3fc51dac5e2af305f71f2f5d1bfc016104cb21c6
Hee Hoon Kim <heehoonkim@Hees-MBP.attlocal.net>
2021-11-13 23:30:37 -0500

    window prompt for # of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //shid
+var charNum;
 var clickBtn = document.getElementById("generate");
 clickBtn.addEventListener("click", function () {
   console.log("Generate button was clicked")
-})
 
+//Ask user how many characters they want
+    var userChoice = window.prompt("How many characters would you like your password to contain?");
+    if (8 > userChoice > 128) {
+      console.log("worked");
+    
+      return;
+    }
+    else if (userChoice === !Number) {
+
+  }
+})
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
