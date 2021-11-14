6df61311bc6501d7fc82b64b237d6c325b997ae3
Hee Hoon Kim <heehoonkim@Hees-MBP.attlocal.net>
2021-11-13 23:50:51 -0500

    added casing confirm prompts

--- Develop/script.js
+++ Develop/script.js
@@ -8,14 +8,23 @@ clickBtn.addEventListener("click", function () {
 
 //Ask user how many characters they want
     var userChoice = window.prompt("How many characters would you like your password to contain?");
-    if (8 > userChoice > 128) {
-      console.log("worked");
-    
+    if (!userChoice) {
+      // console.log("cancelled");
       return;
+    } else if (userChoice < 8 || userChoice > 128) {
+        userChoice = window.prompt("You must choose a number between 8 and 128.")
+        console.log("not num")
+    } else {
+      lowercase = confirm("Would you like to include lowercase characters?");
+      uppercase = confirm("Would you like to include uppercase characters?");
+      numeric = confirm("Would you like to include numeric characters?");
+      special = confirm("Would you like to include special characters?");
+    };
+    if (!lowercase && !uppercase && !numeric && !special) {
+      caseChoice = alert("You must choose at least one type of character.")
+    } else if (lowercase) {
+      
     }
-    else if (userChoice === !Number) {
-
-  }
 })
 // Write password to the #password input
 function writePassword() {
