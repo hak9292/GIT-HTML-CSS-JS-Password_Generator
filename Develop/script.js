fc4b22d30feb84fa8a13b2457ed1fb7d22c84854
Hee Hoon Kim <heehoonkim@Hees-MBP.attlocal.net>
2021-11-13 22:44:38 -0500

    added click function to generate button

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//shid
+var clickBtn = document.getElementById("generate");
+clickBtn.addEventListener("click", function () {
+  console.log("Generate button was clicked")
+})
 
 // Write password to the #password input
 function writePassword() {
