4a71efca8ff8c36fcec32c5f300c9b698d19280b
Hee Hoon Kim <heehoonkim@Hees-MacBook-Pro.local>
2021-11-15 20:05:27 -0500

    changed arrays to strings and used diff function

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//shid
-var charNum;
+var charNum = 0;
+var format = " ";
+var password = " ";
 var clickBtn = document.getElementById("generate");
-clickBtn.addEventListener("click", function () {
-  console.log("Generate button was clicked")
+var lower = "qwertyuiopasdfghjklzxcvbnm";
+//converts "lower" string into uppercase
+var upper = lower.toUpperCase();
+var num = "1234567890";
+var spec = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+function generatePassword() {
+// clickBtn.addEventListener("click", function () {
+  //console.log("Generate button was clicked")
 
 //Ask user how many characters they want
-    var userChoice = window.prompt("How many characters would you like your password to contain?");
-    if (!userChoice) {
-      // console.log("cancelled");
+    charNum = window.prompt("How many characters would you like your password to contain?");
+    if (!charNum) {
+      console.log("cancelled");
       return;
-    } else if (userChoice < 8 || userChoice > 128) {
-        userChoice = window.prompt("You must choose a number between 8 and 128.")
-        console.log("not num")
+    } else if (isNaN(charNum)) {
+        userChoice = window.alert("Password length must be provided as a number.")
+        return;
+    } else if (charNum < 8 || charNum> 128) {
+        charNum = window.alert("You must choose a number between 8 and 128.");
+        return;
+        // console.log("not num");
     } else {
       lowercase = confirm("Would you like to include lowercase characters?");
       uppercase = confirm("Would you like to include uppercase characters?");
       numeric = confirm("Would you like to include numeric characters?");
       special = confirm("Would you like to include special characters?");
-    };
-    if (!lowercase && !uppercase && !numeric && !special) {
-      caseChoice = alert("You must choose at least one type of character.")
-    } else if (lowercase) {
-      
     }
-})
-// Write password to the #password input
+    if (lowercase) {
+      format += lower;
+    }
+    if (uppercase) {
+      format += upper;
+    }
+    if (numeric) {
+      format += num;
+    }
+    if (special) {
+      format += spec;
+    } else if (!lowercase && !uppercase && !numeric && !special) {
+      alert("You must choose at least one type of character.");
+      return;
+    }
+    for (i = 0; i < charNum; i++) {
+      password += format[Math.floor(Math.random() * format.length)];
+    } return password;
+  }
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
